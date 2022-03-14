2c70b3670265db34ecd6ab0dc7e203303251b7e4
LukeJiricek <lukasjiricek@hotmail.com>
2022-03-14 10:27:56 +0100

    abc

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -10,6 +14,3 @@ def index():
 def form():
     return render_template('form.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
-#test
