6ae116c3bc70eb27860dd79026465ceeed9edc05
LukeJiricek <lukasjiricek@hotmail.com>
2022-08-14 13:00:02 +0200

    new

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import ForeignKey
 from app import db
 from sqlalchemy.schema import FetchedValue
 
+
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     link = db.Column(db.String(50), unique=True)
@@ -11,24 +12,28 @@ class Image(db.Model):
     author = db.Column(db.String(50))
     answer = db.relationship("Answer")
 
+
 class Http_request(db.Model):
     request_id = db.Column(db.String(50), primary_key=True, unique=True)
     image_id = db.Column(db.Integer)
     timestamp = db.Column(db.String(100), server_default=FetchedValue())
     attributes = db.Column(db.ARRAY(db.String))
 
+
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    image_id = db.Column(db.Integer, db.ForeignKey('image.id'))
+    image_id = db.Column(db.Integer, db.ForeignKey("image.id"))
     attributes = db.Column(db.ARRAY(db.String))
     timestamp = db.Column(db.String(100), server_default=FetchedValue())
 
+
 class Answer2(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    image_id = db.Column(db.Integer, db.ForeignKey('image.id'))
+    image_id = db.Column(db.Integer, db.ForeignKey("image.id"))
     attributes_true = db.Column(db.ARRAY(db.String))
     attributes_false = db.Column(db.ARRAY(db.String))
     timestamp = db.Column(db.String(100), server_default=FetchedValue())
+
 
 class Attribute(db.Model):
     id = db.Column(db.Integer, primary_key=True)
